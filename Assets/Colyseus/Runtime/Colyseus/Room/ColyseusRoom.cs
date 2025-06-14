0ef97dba61f87dd4d34cd835169f2934999852a5
Tam <tam.vominh@gameloft.com>
2025-06-14 18:32:53 +0700

    debug log

--- Assets/Colyseus/Runtime/Colyseus/Room/ColyseusRoom.cs
+++ Assets/Colyseus/Runtime/Colyseus/Room/ColyseusRoom.cs
@@ -210,10 +210,12 @@ namespace Colyseus
 	        room ??= this;
 	        room.Connection = colyseusConnection;
 
+            room.Connection.OnOpen += () => Debug.Log($"websocket {RoomId} connected");
 	        room.Connection.OnClose += code =>
 	        {
 		        if (devModeCloseCallback == null || code == 1006)
 		        {
+                    Debug.Log($"websocket {RoomId} closed: {code}");
 			        room.OnLeave?.Invoke(code);
 		        }
 		        else
